0b5be5eeb847710e5f546e218a1b0cb142ea11f5
Vikash Roy <98211278+chiggosVikash@users.noreply.github.com>
2024-08-20 23:43:32 +0530

    Delete dist directory

--- dist/controllers/account_controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AccountController = void 0;
-class AccountController {
-}
-exports.AccountController = AccountController;
